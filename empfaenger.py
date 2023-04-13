8021d20d54a00e47361cd37f07501cfb1c387aab
psschaefer <62019055+psschaefer@users.noreply.github.com>
2023-04-13 18:57:19 +0200

    Add files via upload
    Sender und Receiver für UDP file übertragung, korrektheit anhand sequenznummern und md5

---
+++ empfaenger.py
@@ -0,0 +1,73 @@
+import socket
+import hashlib
+import os
+import struct
+
+# Definiere Konstanten
+BUFFER_SIZE = 1024
+UDP_IP = 'localhost' #  127.0.0.1'
+UDP_PORT = 5005
+
+# Erstelle UDP-Socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.bind((UDP_IP, UDP_PORT))
+
+# Empfangen des ersten Pakets
+header, addr = sock.recvfrom(BUFFER_SIZE*2+10)
+trans_id, seq_num, max_seq= struct.unpack('!HLL', header[:10])
+filename = header[10:].decode()
+
+
+
+
+with open(filename, 'wb') as f:
+    seq_num=1
+    # Empfangen der Datenpakete
+    while seq_num < max_seq:
+        data, addr = sock.recvfrom(BUFFER_SIZE+6)
+        trans_id_recv, seq_num_recv  = struct.unpack('!HL', data[:6])
+        
+        # Überprüfe Transmission ID und Sequenznummer
+        if trans_id != trans_id_recv or seq_num != seq_num_recv:
+            print('Falsches Paket empfangen')
+            break
+        
+        # Schreibe Daten in Datei
+        f.write(data[6:])
+        if seq_num > max_seq:
+            break
+        
+        seq_num +=1
+
+        
+
+    # Empfangen des letzten Pakets mit dem MD5-Hash der Datei
+    data, addr = sock.recvfrom(BUFFER_SIZE+6)
+    trans_id_recv, seq_num= struct.unpack('!HL', data[:6])
+    file_md5_recv = data[6:].hex()
+    
+
+
+    # Überprüfe Transmission ID, Sequenznummer und MD5-Hash
+    if trans_id != trans_id_recv or seq_num != max_seq :
+        print('Fehler beim Empfangen der Datei')
+   
+
+
+f.close()
+
+
+with open(filename, 'rb') as f:
+    data_for_hash_compare = f.read()
+md5 = hashlib.md5(data_for_hash_compare).hexdigest()
+if md5 == file_md5_recv :
+    print('Korrekter Hash')
+else:
+    print('Falscher Hash')
+
+
+print(md5)
+print(file_md5_recv)
+
+f.close()
+sock.close()
---
+++ sender.py
@@ -0,0 +1,57 @@
+import socket
+import hashlib
+import os
+import struct
+
+# Definiere Konstanten
+BUFFER_SIZE = 1024
+UDP_IP = 'localhost'    #127.0.0.1'
+UDP_PORT = 5005
+
+# Wähle die zu sendende Datei
+filename = 'example2.txt'
+
+# Erstelle UDP-Socket
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+# Bestimme die maximale Sequenznummer
+
+max_seq = os.path.getsize(filename) // BUFFER_SIZE
+if os.path.getsize(filename) % BUFFER_SIZE != 0:
+    max_seq += 1
+
+
+#Für letztes Paket mit md5
+max_seq +=1
+
+# Sende das erste Paket mit der Dateiinformation
+trans_id = 1234 # Wähle eine Transmission ID
+seq_num = 0
+filename_len = len(filename)
+header = struct.pack('!HLL', trans_id, seq_num, max_seq) + filename.encode()
+sock.sendto(header, (UDP_IP, UDP_PORT))
+
+
+
+
+# Sende die Datenpakete
+with open(filename, 'rb') as f:
+    for i in range(max_seq-1):
+        data = f.read(BUFFER_SIZE)
+        seq_num += 1
+        packet = struct.pack('!HL', trans_id, seq_num) + data
+        sock.sendto(packet, (UDP_IP, UDP_PORT))
+
+
+
+
+# Sende das letzte Paket mit dem MD5-Hash
+with open(filename, 'rb') as f:
+    data = f.read()
+
+md5 = hashlib.md5(data).digest()
+packet = struct.pack('!HL', trans_id, max_seq) + md5
+sock.sendto(packet, (UDP_IP, UDP_PORT))
+print()
+# Schließe den Socket
+sock.close()
